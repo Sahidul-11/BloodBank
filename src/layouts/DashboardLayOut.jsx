ec1f9444e4ae4715780e4a8740511e6a8e51098c
sahidul-11 <mdsahidulislam161113@gmail.com>
2024-11-04 18:43:54 +0600

    dashboard design changed

--- src/layouts/DashboardLayOut.jsx
+++ src/layouts/DashboardLayOut.jsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom';
 
 const DashboardLayOut = () => {
     return (
-        <div className="relative md:flex">
-            <div className="absolute md:relative left-0 top-0 w-2/3 md:w-1/3 lg:1/4 z-50">
+        <div className=" relative  md:flex">
+            <div className=" absolute lg:relative  left-0 top-0 lg:1/4 z-50">
                 <Sidebars />
             </div>
 
